4471c0c04069e745087d8146392a80786517abe3
Daniel Abrao <danielpaladar@gmail.com>
2020-09-06 19:50:07 -0300

    Add error handlers

--- BlockchainController.js
+++ BlockchainController.js
@@ -17,6 +17,7 @@ class BlockchainController {
         this.submitStar();
         this.getBlockByHash();
         this.getStarsByOwner();
+        this.validateChain();
     }
 
     // Enpoint to Get a Block by Height (GET Endpoint)
@@ -115,6 +116,13 @@ class BlockchainController {
                 return res.status(500).send("Block Not Found! Review the Parameters!");
             }
             
+        });
+    }
+    // This endpoint allows you to request the list of Stars registered by an owner
+    validateChain() {
+        this.app.get("/validateChain", async (req, res) => {
+            return res.status(200).json(await this.blockchain.validateChain());
+
         });
     }
 
